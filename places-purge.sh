f668522721a8539ec09d5cad678de6cc0df78173
@apple-fritter <125221355+apple-fritter@users.noreply.github.com>
2023-04-29 17:27:50 -0700

    Update places-purge.sh

--- places-purge.sh
+++ places-purge.sh
@@ -6,10 +6,14 @@ PLACES_DB=$(find ~/.mozilla/firefox -name places.sqlite -print -quit)
 # Stop Firefox if it's running
 pgrep firefox >/dev/null 2>&1 && firefox --quit >/dev/null 2>&1
 
-# Remove the contents of the places.sqlite file
+# Define the tables to delete (excluding sqlite_sequence)
+tables=("moz_historyvisits" "moz_inputhistory" "moz_bookmarks" "moz_bookmarks_roots" "moz_keywords" "moz_places" "moz_anno_attributes" "moz_annos" "moz_items_annos" "moz_meta" "moz_origins" "moz_sync")
+
 echo "Removing contents of places.sqlite"
-for table in moz_historyvisits moz_inputhistory moz_bookmarks moz_bookmarks_roots moz_keywords moz_places; do
-    sqlite3 "$PLACES_DB" "DELETE FROM $table;"
+for table in "${tables[@]}"; do
+    if [[ "$table" != "sqlite_sequence" ]]; then
+        sqlite3 "$PLACES_DB" "DELETE FROM $table;"
+    fi
 done
 
 echo "Done!"
